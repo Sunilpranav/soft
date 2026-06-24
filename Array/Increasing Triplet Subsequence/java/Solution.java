30ffc9e4928839b61f338307cb0a056296b69071
Sunil Pranav.S <sunilpranavbe@gmail.com>
2026-06-25 00:06:42 +0530

    feat: Add Increasing Triplet Subsequence

--- Array/Increasing Triplet Subsequence/java/Solution.java
+++ Array/Increasing Triplet Subsequence/java/Solution.java
@@ -2,17 +2,23 @@ class Solution {
     public boolean increasingTriplet(int[] nums) {
         int i=0,j=1,k=2;
         boolean op=false;
-        while(k<nums.length && j<nums.length-1 && i<nums.length-2)
+        while(k<nums.length)
         {
-            if(nums[i]<nums[j] && nums[j]<nums[k])
+            while(j<nums.length-1)
             {
-                op=true;
+                while(i<nums.length-2)
+                {
+                     if(nums[i]<nums[j] && nums[j]<nums[k])
+                    {
+                        op=true;
                 // System.out.println(i);
                 // System.out.println(j);
                 // System.out.println(k);
+                    } 
+                    i++;
+                }
+                j++;
             }
-            i++;
-            j++;
             k++;
         }
         return op;
