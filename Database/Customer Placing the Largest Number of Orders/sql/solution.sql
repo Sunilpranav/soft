b01a6195073e48691feae3e0f0b8568a1ffb2812
Sunil Pranav.S <sunilpranavbe@gmail.com>
2026-06-30 15:25:13 +0530

    feat: Add Customer Placing the Largest Number of Orders

--- Database/Customer Placing the Largest Number of Orders/sql/solution.sql
+++ Database/Customer Placing the Largest Number of Orders/sql/solution.sql
@@ -1,5 +1,5 @@
 # Write your MySQL query statement below
 select customer_number from Orders
 group by customer_number
-#having max(customer_number)
+having count(customer_number)
 order by customer_number desc limit 1;
