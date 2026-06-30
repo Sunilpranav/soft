d44ade1903d88d457671d8f0092521de614d5f78
Sunil Pranav.S <sunilpranavbe@gmail.com>
2026-06-30 13:01:05 +0530

    feat: Add Relative Sort Array

---
+++ Array/Relative Sort Array/java/Solution.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int[] relativeSortArray(int[] arr1, int[] arr2) {
+        int left=0,right=0;
+        int i=0;
+        while(i<arr2.length)
+        {
+            while(right<arr1.length)
+            {
+                if(arr2[i]==arr1[right])
+                {
+                    int temp=arr1[left];
+                    arr1[left]=arr1[right];
+                    arr1[right]=temp;
+                    left++;
+                }
+                right++;
+            }
+            i++;
+            right=left;
+        }
+        for(int j=left;j<arr1.length;j++)
+        {
+            if(arr1[j-1]>arr1[j])
+            {
+                int temp1=arr1[j-1];
+                arr1[j-1]=arr1[j];
+                arr1[j]=temp1;
+            }
+        }
+        return arr1;
+    }
+}
