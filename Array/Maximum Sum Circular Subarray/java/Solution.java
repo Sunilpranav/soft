435f49ce4eeeef14e27a4d2899e986aec0787714
Sunil Pranav.S <sunilpranavbe@gmail.com>
2026-07-01 12:48:51 +0530

    feat: Add Maximum Sum Circular Subarray

--- Array/Maximum Sum Circular Subarray/java/Solution.java
+++ Array/Maximum Sum Circular Subarray/java/Solution.java
@@ -1,20 +1,45 @@
 class Solution {
     public int maxSubarraySumCircular(int[] nums) {
-        int max=Integer.MIN_VALUE;
-        for(int i=0;i<nums.length;i++)
-        {
-            
-            for(int j=1;j<=nums.length;j++)
-            {
-                int sum=0;
-                for(int k=0;k<j;k++)
-                {
-                    sum+=nums[(i+k)%nums.length];
-                }
-                max=Math.max(sum,max);
-            }
-           
+        int total = 0;
+
+        for(int x : nums)
+            total += x;
+
+        int maxNormal = kadaneMax(nums);
+        if (maxNormal < 0)return maxNormal;
+        int minSub = kadaneMin(nums);
+
+        int maxCircular = total - minSub;
+
+        return Math.max(maxNormal,maxCircular);
+    }
+    static int kadaneMax(int arr[]) {
+
+        int current = arr[0];
+        int max = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+
+            current = Math.max(arr[i], current + arr[i]);
+
+            max = Math.max(max, current);
         }
+
         return max;
+    }
+
+    static int kadaneMin(int arr[]) {
+
+        int current = arr[0];
+        int min = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+
+            current = Math.min(arr[i], current + arr[i]);
+
+            min = Math.min(min, current);
+        }
+
+        return min;
     }
 }
