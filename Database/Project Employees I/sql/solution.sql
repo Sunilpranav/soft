fd4de0e729735984baabb8c8292429709e83e056
Sunil Pranav.S <sunilpranavbe@gmail.com>
2026-07-01 16:17:33 +0530

    feat: Add Project Employees I

--- Database/Project Employees I/sql/solution.sql
+++ Database/Project Employees I/sql/solution.sql
@@ -1,5 +1,5 @@
 # Write your MySQL query statement below
-select project_id, avg(experience_years)as average_years from 
+select project_id, round(avg(experience_years),2)as average_years from 
 Project p inner join Employee e on
 p.employee_id=e.employee_id
 group by p.project_id;
