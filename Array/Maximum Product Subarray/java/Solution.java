a3b87cc296333613ff1d22c606005318584f12f4
Sunil Pranav.S <sunilpranavbe@gmail.com>
2026-07-01 14:05:54 +0530

    feat: Add Maximum Product Subarray

--- Array/Maximum Product Subarray/java/Solution.java
+++ Array/Maximum Product Subarray/java/Solution.java
@@ -1,19 +1,29 @@
 class Solution {
     public int maxProduct(int[] nums) {
         int max=Integer.MIN_VALUE;
-        int pro=1;
         for(int i=0;i<nums.length;i++)
         {
-            pro*=nums[i];
-            if(pro>max)
+            int pro=1;
+            for(int j=0;j<nums.length;j++)
             {
-                max=pro;
+                pro*=nums[j];
+                max=Math.max(pro,max);
             }
-            // if(pro<0 && )
-            // {
-            //     pro=-1;
-            // }
         }
         return max;
+        // int pro=1;
+        // for(int i=0;i<nums.length;i++)
+        // {
+        //     pro*=nums[i];
+        //     if(pro>max)
+        //     {
+        //         max=pro;
+        //     }
+        //     if(pro==0 )
+        //     {
+        //         pro=-1;
+        //     }
+        // }
+        // return max;
     }
 }
