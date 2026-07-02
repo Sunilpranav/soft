46a038287c5a74bf45b449bd67d994966d3b3e0a
Sunil Pranav.S <sunilpranavbe@gmail.com>
2026-07-02 10:59:04 +0530

    feat: Add Two Sum II - Input Array Is Sorted

--- Array/Two Sum II - Input Array Is Sorted/java/Solution.java
+++ Array/Two Sum II - Input Array Is Sorted/java/Solution.java
@@ -1,24 +1,22 @@
 class Solution {
     public int[] twoSum(int[] numbers, int target) {
         int left=0,right=numbers.length-1;
-        int ans[]=new int[2];
-        int i=0;
         while(left<numbers.length)
         {
-            right=numbers.length-1;
-        while(left<right)
-        {
-            if((numbers[left]+numbers[right]==target))
-            {
-                ans[i]=left+1;
-                i++;
-                ans[i]=right+1;
-            }
-            right--;
+           if(numbers[left]+numbers[right]==target)
+           {
+                return new int[]{left+1,right+1};
+           }
+           else if(numbers[left]+numbers[right]>target)
+           {
+                right--;
+           }
+           else
+           {
+                left++;
+           }
         }
-            left++;
-        }
-        return ans;
+        return new int[]{};
 
     }
 }
