776f10504feddfcb63491201b032278fa26cbe34
Sunil Pranav.S <sunilpranavbe@gmail.com>
2026-07-02 12:45:18 +0530

    feat: Add Longest Substring of One Repeating Character

--- Array/Longest Substring of One Repeating Character/java/Solution.java
+++ Array/Longest Substring of One Repeating Character/java/Solution.java
@@ -1,27 +1,127 @@
 class Solution {
+
+    class Node {
+        char leftChar;
+        char rightChar;
+
+        int prefix;
+        int suffix;
+        int longest;
+        int len;
+    }
+
+    Node[] tree;
+    char[] arr;
+
     public int[] longestRepeating(String s, String queryCharacters, int[] queryIndices) {
-        StringBuilder sb=new StringBuilder(s);
-        int ans[]=new int[queryCharacters.length()];
-        for(int i=0;i<queryIndices.length;i++)
-        {
-            sb.setCharAt(queryIndices[i],queryCharacters.charAt(i));
-            HashSet<Integer>h=new HashSet<>();
-            int count=1;
-            int max=1;
-            for(int j=1;j<sb.length();j++)
-            {
-                if(sb.charAt(j-1)==sb.charAt(j))
-                {
-                    count++;
-                }
-                else
-                {
-                    count=1;
-                }
-                max=Math.max(max,count);
-            }
-            ans[i]=max;
+
+        int n = s.length();
+
+        arr = s.toCharArray();
+
+        tree = new Node[4 * n];
+
+        for (int i = 0; i < tree.length; i++)
+            tree[i] = new Node();
+
+        build(1, 0, n - 1);
+
+        int[] ans = new int[queryIndices.length];
+
+        for (int i = 0; i < queryIndices.length; i++) {
+
+            update(1, 0, n - 1,
+                    queryIndices[i],
+                    queryCharacters.charAt(i));
+
+            ans[i] = tree[1].longest;
         }
-       return ans;
+
+        return ans;
+    }
+
+    private void build(int idx, int l, int r) {
+
+        if (l == r) {
+
+            tree[idx].leftChar = arr[l];
+            tree[idx].rightChar = arr[l];
+            tree[idx].prefix = 1;
+            tree[idx].suffix = 1;
+            tree[idx].longest = 1;
+            tree[idx].len = 1;
+
+            return;
+        }
+
+        int mid = (l + r) / 2;
+
+        build(idx * 2, l, mid);
+        build(idx * 2 + 1, mid + 1, r);
+
+        merge(idx);
+    }
+
+    private void update(int idx, int l, int r, int pos, char ch) {
+
+        if (l == r) {
+
+            arr[pos] = ch;
+
+            tree[idx].leftChar = ch;
+            tree[idx].rightChar = ch;
+            tree[idx].prefix = 1;
+            tree[idx].suffix = 1;
+            tree[idx].longest = 1;
+
+            return;
+        }
+
+        int mid = (l + r) / 2;
+
+        if (pos <= mid)
+            update(idx * 2, l, mid, pos, ch);
+        else
+            update(idx * 2 + 1, mid + 1, r, pos, ch);
+
+        merge(idx);
+    }
+
+    private void merge(int idx) {
+
+        Node left = tree[idx * 2];
+        Node right = tree[idx * 2 + 1];
+        Node cur = tree[idx];
+
+        cur.len = left.len + right.len;
+
+        cur.leftChar = left.leftChar;
+        cur.rightChar = right.rightChar;
+
+        cur.prefix = left.prefix;
+
+        if (left.prefix == left.len &&
+                left.rightChar == right.leftChar) {
+
+            cur.prefix += right.prefix;
+        }
+
+        cur.suffix = right.suffix;
+
+        if (right.suffix == right.len &&
+                left.rightChar == right.leftChar) {
+
+            cur.suffix += left.suffix;
+        }
+
+        cur.longest = Math.max(left.longest, right.longest);
+
+        if (left.rightChar == right.leftChar) {
+
+            cur.longest = Math.max(
+                    cur.longest,
+                    left.suffix + right.prefix
+            );
+        }
     }
 }
