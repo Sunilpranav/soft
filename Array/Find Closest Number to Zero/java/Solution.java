3747855bbf727d46afbc5fa422e51172e3bfa756
Sunil Pranav.S <sunilpranavbe@gmail.com>
2026-07-27 14:11:01 +0530

    feat: Add Find Closest Number to Zero

--- Array/Find Closest Number to Zero/java/Solution.java
+++ Array/Find Closest Number to Zero/java/Solution.java
@@ -1,15 +1,20 @@
 class Solution {
     public int findClosestNumber(int[] nums) {
         int min=Integer.MAX_VALUE;
+        int n=0,ans=0;
         for(int i=0;i<nums.length;i++)
         {
             //int n=nums[i];
-            if(nums[i]<0)
+            if(Math.abs(nums[i]-0)<=min)
             {
-                nums[i]=nums[i]*-1;
+                min=Math.abs(nums[i]-0);
+                // if(n==nums[i])
+                // {
+                //     ans=Math.max(n,nums[i]);
+                // }
+                n=nums[i];
             }
-            min=Math.min(min,Math.abs(0-nums[i]));
         }
-        return min;
+        return n;
     }
 }
