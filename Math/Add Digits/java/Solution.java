a0d7f398965e5b9848b5abfc3b41346b210498c6
Sunil Pranav.S <sunilpranavbe@gmail.com>
2026-07-30 19:39:06 +0530

    feat: Add Add Digits

---
+++ Math/Add Digits/java/Solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int addDigits(int num) {
+        int sum=0;
+        while(num>10)
+        {
+            sum=0;
+            int n=num;
+            while(n>0)
+            {
+                int x =n%10;
+                sum+=x;
+                n/=10;
+            }
+            num=sum;
+        }
+        return sum;
+    }
+}
--- README.md
+++ README.md
@@ -16,6 +16,7 @@ A professionally structured collection of topic-wise DSA solutions, optimized co
 
 | # | Problem | Difficulty |
 |---|---------|------------|
+| 258 | [Add Digits](./Math/Add%20Digits/) | Easy |
 | 2894 | [Divisible and Non-divisible Sums Difference](./Math/Divisible%20and%20Non-divisible%20Sums%20Difference/) | Easy |
 
 </details>
