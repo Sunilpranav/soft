054c16953fc8bd89c7e7c2bdc216d4cc286124e8
Sunil Pranav.S <sunilpranavbe@gmail.com>
2026-07-31 09:57:12 +0530

    feat: Add Merge Sorted Array

--- Array/Merge Sorted Array/java/Solution.java
+++ Array/Merge Sorted Array/java/Solution.java
@@ -1,7 +1,8 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int l=0;
-        for(int i=0;i<nums1.length;i++)
+        //int l=0;
+        System.out.println(Arrays.toString(nums1));
+        for(int i=0,l=0;i<nums1.length &&l<n ;i++)
         {
             if(nums1[i]==0)
             {
